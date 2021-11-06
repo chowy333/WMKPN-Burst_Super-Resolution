5f81d33657174acd025f5376da0ff740541b8258
Woo Yeong CHO <46465539+chowy333@users.noreply.github.com>
2021-11-06 16:55:43 +0900

    Update test_WMKPN.sh

--- scripts/test_WMKPN.sh
+++ scripts/test_WMKPN.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 python -u test.py\
 	--test_path '/SSD/samsung/wycho/datasets/Zurich_Public/' --save_output_path "./output/WMKPN_attn"\
-    	--model WMKPN --pretrained_model "./ckpts/first/WMKPN-bs_8-64-x4_model_best.path" --bias --scale 4 --gpu_id 0\
+    	--model WMKPN --pretrained_model "./ckpts/WMKPN-bs_8-64-x4_model_best.path" --bias --scale 4 --gpu_id 0\
             --color --blind_est --sep_conv --channel_att --spatial_att
 
 # if run out of memory, lower batch_size down
+# make sure that your "test_path" is correct.
